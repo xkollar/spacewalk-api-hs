93972cb2431529e1853956ac6124ce32516121cb
Matej Kollar <mkollar@redhat.com>
2015-07-20 11:17:24 +0200

    Docs

--- src/Spacewalk/ApiInternal.hs
+++ src/Spacewalk/ApiInternal.hs
@@ -12,11 +12,13 @@ import Spacewalk.ApiTypes
 voidInt :: Functor f => f Int -> f ()
 voidInt = void
 
+-- | Use this for unauthenticated API calls.
 swRemoteBase :: Remote a => String -> (a -> IO b) -> SpacewalkRPC b
 swRemoteBase s m = do
     u <- asks apiurl
     liftIO $ m (remote u s)
 
+-- | Use this for authenticated API calls.
 swRemote :: Remote (Key -> a) => String -> (a -> IO b) -> SpacewalkRPC b
 swRemote s m = do
     mk <- gets key
