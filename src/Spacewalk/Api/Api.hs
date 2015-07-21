6eb5ad3dc488579bebb7bcad12a283ee8395299a
Matej Kollar <mkollar@redhat.com>
2015-07-21 22:31:17 +0200

    Work

--- src/Spacewalk/Api/Api.hs
+++ src/Spacewalk/Api/Api.hs
@@ -1,1 +1,13 @@
-module Spacewalk.Api.Api () where
+module Spacewalk.Api.Api
+    ( getVersion
+    , systemVersion
+    ) where
+
+import Spacewalk.ApiTypes
+import Spacewalk.ApiInternal
+
+getVersion :: SpacewalkRPC String
+getVersion = swRemoteBase "api.getVersion" id
+
+systemVersion :: SpacewalkRPC String
+systemVersion = swRemoteBase "api.systemVersion" id
